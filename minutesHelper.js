ecf6ff7ab968a92453673b2986752e64a918495c
Orangeat <nsorangepv@gmail.com>
2021-09-01 03:56:47 +0900

    [update] Set text data when the cursor is on the table

--- minutesHelper.js
+++ minutesHelper.js
@@ -4,6 +4,21 @@ function showDialog(){
 }
 
 function send(form) {
-  const remark = form.remark
-  // DocumentApp.getActiveDocument().getBody().insertParagraph(0, remark);
+  const [person, remark] = form.remark.split(' ', 2)
+  const doc = DocumentApp.getActiveDocument();
+  const TABLETYPE = DocumentApp.ElementType.TABLE;
+  const TABLECELLROW = DocumentApp.ElementType.TABLE_ROW;
+  const body = doc.getBody();
+  const cursor = doc.getCursor();
+  const mainTable = body.findElement(TABLETYPE, null).getElement;
+  const cursorTableRow = cursor.getElement().getParent().getParent();
+  if (cursorTableRow.getType() == TABLECELLROW) {
+    const cursorTable = cursorTableRow.getParent();
+    cursorTableRow.getCell(0).setText(person);
+    cursorTableRow.getCell(1).setText(remark);
+    const cursorRowNumber = cursorTable.getChildIndex(cursorTableRow);
+    const nextTableRow = cursorTable.insertTableRow(cursorRowNumber + 1);
+    nextTableRow.insertTableCell(0);
+    doc.setCursor(doc.newPosition(nextTableRow.insertTableCell(1), 0));
+  }
 }
