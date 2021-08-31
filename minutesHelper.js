098c3c3627bb9a89f10011bcf32628d0f754050d
Orangeat <nsorangepv@gmail.com>
2021-09-01 04:17:33 +0900

    [update] Set text data also when the cursor is outside of the table

--- minutesHelper.js
+++ minutesHelper.js
@@ -4,21 +4,34 @@ function showDialog(){
 }
 
 function send(form) {
-  const [person, remark] = form.remark.split(' ', 2)
+  const OFFSET_ROW_INDEX = 5;
+
+  const [person, remark] = form.remark.split(' ', 2);
   const doc = DocumentApp.getActiveDocument();
   const TABLETYPE = DocumentApp.ElementType.TABLE;
   const TABLECELLROW = DocumentApp.ElementType.TABLE_ROW;
   const body = doc.getBody();
   const cursor = doc.getCursor();
-  const mainTable = body.findElement(TABLETYPE, null).getElement;
-  const cursorTableRow = cursor.getElement().getParent().getParent();
-  if (cursorTableRow.getType() == TABLECELLROW) {
-    const cursorTable = cursorTableRow.getParent();
-    cursorTableRow.getCell(0).setText(person);
-    cursorTableRow.getCell(1).setText(remark);
-    const cursorRowNumber = cursorTable.getChildIndex(cursorTableRow);
-    const nextTableRow = cursorTable.insertTableRow(cursorRowNumber + 1);
-    nextTableRow.insertTableCell(0);
-    doc.setCursor(doc.newPosition(nextTableRow.insertTableCell(1), 0));
+  const mainTable = body.findElement(TABLETYPE, null).getElement();
+  let flag = false;
+  if (cursor) {
+    const cursorTableRow = cursor.getElement().getParent().getParent();
+    if (cursorTableRow.getType() == TABLECELLROW) {
+      const cursorTable = cursorTableRow.getParent();
+      cursorTableRow.getCell(0).setText(person);
+      cursorTableRow.getCell(1).setText(remark);
+      const nextTableRow = cursorTable.insertTableRow(cursorTable.getChildIndex(cursorTableRow) + 1);
+      nextTableRow.insertTableCell(0);
+      doc.setCursor(doc.newPosition(nextTableRow.insertTableCell(1), 0));
+      flag = true;
+    } 
+  }
+  if (flag == false) {
+    const lastNewRow = mainTable.appendTableRow();
+    const lastRowInd = mainTable.getChildIndex(lastNewRow) - 1;
+    mainTable.getCell(lastRowInd - OFFSET_ROW_INDEX, 0).setText(person);
+    mainTable.getCell(lastRowInd - OFFSET_ROW_INDEX, 1).setText(remark);
+    lastNewRow.insertTableCell(0);
+    doc.setCursor(doc.newPosition(lastNewRow.insertTableCell(1), 0));
   }
 }
